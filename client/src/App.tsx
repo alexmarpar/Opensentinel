14230a2a7cd3bd788dfd4c22c2faf38c35771443
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-08-17 01:35:28 +0200

    OpenSentinel 1.0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Topbar from "./components/Topbar";
-import { Command } from "@tauri-apps/plugin-shell";
-
 
 import Dashboard from "./pages/Dashboard";
 import SSHDevices from "./pages/SSHDevices";
-
-const command = Command.sidecar("binaries/backend");
-
-await command.spawn();
 
 export default function App() {
     return (
