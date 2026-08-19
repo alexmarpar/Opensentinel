c603de09dec8c35eafe521687be40db0b7395eb6
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-08-19 05:38:18 +0200

    Routes for astro.config.mjs added

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexmarpar.github.io',
+  base: '/Opensentinel/',
   vite: {
     plugins: [tailwindcss()]
   },
