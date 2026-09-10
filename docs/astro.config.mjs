62fcd6324edd172a475feacd08bab6cb9591631a
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-09-10 20:43:22 +0200

    Updated routes for develoment and github pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import starlight from '@astrojs/starlight';
 // https://astro.build/config
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    
   },
+  site: 'https://alexmarpar.github.io',
+	base: '/Opensentinel',
+  integrations: [
+    starlight({
+      title: 'OpenSentinel',
+    }),
+  ],
 });
