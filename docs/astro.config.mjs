c962b64ba2dcadf2af0fca25f22f7071967211a1
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-09-10 20:15:02 +0200

    Fixed OpenSentinel website render

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexmarpar.github.io',
-  base: '/Opensentinel',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  integrations: [starlight({ title: 'OpenSentinel' })]
 });
